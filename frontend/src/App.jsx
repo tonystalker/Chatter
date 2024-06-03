6f591963f2bc23c0da340759028e8586ef33ff7e
Ayush Tripathi <707ayushtripathi@gmail.com>
2024-06-03 10:37:15 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
 import React from "react"
+import { StreamChat } from "stream-chat"
+import { Chat } from "stream-chat-react"
+import Cookies from "universal-cookie"
+import env from "react-dotenv"
+
+const apiKey = env.STREAM_API_KEY
 
 const App = () => {
     return <div>Chatter</div>
