69025cc606ddc4afa5ac266b3d35d92fc4502e3e
Ayush Tripathi <707ayushtripathi@gmail.com>
2024-06-03 10:45:15 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,17 @@ import Cookies from "universal-cookie"
 import env from "react-dotenv"
 
 const apiKey = env.STREAM_API_KEY
+const client = StreamChat.getInstance(apiKey)
 
 const App = () => {
-    return <div>Chatter</div>
+    return (
+        <div className="app_wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelList />
+                <Channel />
+            </Chat>
+        </div>
+    )
 }
 
 export default App
